e3ec028d4cff3df109a48e52708200b15af684fd
JobiJoba <Johann.baziret@gmail.com>
2022-04-11 10:15:38 +0700

    remove some useless config

--- apps/testApp/src/main.tsx
+++ apps/testApp/src/main.tsx
@@ -3,8 +3,6 @@ import './index.css'
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 
-import ReactDOM from 'react-dom'
-
 import App from './App'
 
 const root = createRoot(document.getElementById('root')!)
