562bda9f31d9c8f23b4f0d9c62024722d30cd08a
JobiJoba <Johann.baziret@gmail.com>
2022-04-11 12:40:37 +0700

    remove cypress from test ci

--- apps/testApp/jest.config.ts
+++ apps/testApp/jest.config.ts
@@ -8,4 +8,5 @@ export default {
         '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
     },
     setupFilesAfterEnv: ['<rootDir>/src/jest.setup.ts'],
+    testMatch: ['**/src/**/?(*.)+(spec|test).[jt]s?(x)'],
 }
