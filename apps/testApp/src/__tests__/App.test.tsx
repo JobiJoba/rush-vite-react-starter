ca8f3f355cfce8fb8f0e7ba36d40cb23264b0360
JobiJoba <Johann.baziret@gmail.com>
2022-04-11 09:31:31 +0700

    eslint, prettier and jest

--- apps/testApp/src/__tests__/App.test.tsx
+++ apps/testApp/src/__tests__/App.test.tsx
@@ -1,26 +1,26 @@
 // Imports
-import { render, screen } from "@testing-library/react"
-import user from "@testing-library/user-event"
+import { render, screen } from '@testing-library/react'
+import user from '@testing-library/user-event'
 
 // To Test
-import App from "../App"
+import App from '../App'
 
 // Tests
-test("Renders main page correctly", async () => {
-  // Setup
-  render(<App />)
-  const buttonCount = await screen.findByRole("button")
+test('Renders main page correctly', async () => {
+    // Setup
+    render(<App />)
+    const buttonCount = await screen.findByRole('button')
 
-  // Pre Expecations
-  expect(buttonCount.innerHTML).toBe("count is: 0")
-  // Instead of:
-  // expect(codeCount).toBeNull();
+    // Pre Expecations
+    expect(buttonCount.innerHTML).toBe('count is: 0')
+    // Instead of:
+    // expect(codeCount).toBeNull();
 
-  // Init
-  user.click(buttonCount)
-  user.click(buttonCount)
+    // Init
+    user.click(buttonCount)
+    user.click(buttonCount)
 
-  // Post Expectations
-  expect(buttonCount.innerHTML).toBe("count is: 2")
-  expect(await screen.queryByText(/count is:/)).toBeInTheDocument()
+    // Post Expectations
+    expect(buttonCount.innerHTML).toBe('count is: 2')
+    expect(await screen.queryByText(/count is:/)).toBeInTheDocument()
 })
